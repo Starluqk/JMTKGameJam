07ae3d86fb4f11a2b96a5b825d05ee27ca13bdf4
Ové <samuele.veltri06@gmail.com>
2026-07-22 23:34:51 +0200

    V0.01 - UnityScene

---
+++ Assets/Scripts/Player/PlayerMovements.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovements : MonoBehaviour
+{
+    [Header("Déplacement")]
+    [SerializeField] private float moveSpeed = 6f;
+
+    private Rigidbody2D rb;
+    private Vector2 movementInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        movementInput = new Vector2(moveX, moveY).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = movementInput * moveSpeed;
+    }
+}
