88915c0fb0d0b567d5277f1b63b8ce8b8f6c672c
Ové <samuele.veltri06@gmail.com>
2026-07-23 15:12:08 +0200

    V0.17 - item throw

--- Assets/Scripts/Player/ItemGrab.cs
+++ Assets/Scripts/Player/ItemGrab.cs
@@ -6,15 +6,17 @@ public class ItemGrabber : MonoBehaviour
     [SerializeField] private Transform holdPoint;
     [SerializeField] private float grabRadius = 1.5f;
 
-    [Header("Physique d'Attraction")]
+    [Header("Physique d'Attraction & Lancer")]
     [Range(0.01f, 1f)]
     [SerializeField] private float smoothTime = 0.1f;
     [SerializeField] private float defaultItemDamping = 10f;
+    [SerializeField] private float throwForce = 12f;
 
     [Header("Animation")]
     [SerializeField] private Animator animator;
     [SerializeField] private string isCarryingBool = "IsCarrying";
     [SerializeField] private string releaseTrigger = "Release";
+    [SerializeField] private string throwTrigger = "Throw";
 
     private Rigidbody2D grabbedRb;
     private Vector2 currentVelocity = Vector2.zero;
@@ -35,6 +37,11 @@ public class ItemGrabber : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             ReleaseItem();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ThrowItem();
         }
     }
 
@@ -91,6 +98,35 @@ public class ItemGrabber : MonoBehaviour
             {
                 animator.SetBool(isCarryingBool, false);
                 animator.SetTrigger(releaseTrigger);
+            }
+        }
+    }
+
+    private void ThrowItem()
+    {
+        if (grabbedRb != null)
+        {
+            Rigidbody2D rbToThrow = grabbedRb;
+
+            grabbedRb.linearDamping = defaultItemDamping;
+            grabbedRb = null;
+
+            Vector2 throwDirection = transform.right;
+
+            rbToThrow.AddForce(throwDirection * throwForce, ForceMode2D.Impulse);
+
+            if (animator != null)
+            {
+                animator.SetBool(isCarryingBool, false);
+
+                if (!string.IsNullOrEmpty(throwTrigger))
+                {
+                    animator.SetTrigger(throwTrigger);
+                }
+                else
+                {
+                    animator.SetTrigger(releaseTrigger);
+                }
             }
         }
     }
