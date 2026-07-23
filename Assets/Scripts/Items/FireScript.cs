3acca21bb90589875af4e8709482af03c4f7b72f
Nicolay Coretin <corentin.nicolay@student.hepl.be>
2026-07-23 16:42:36 +0200

    V0.21- Nav mesh + correction feu

--- Assets/Scripts/Items/FireScript.cs
+++ Assets/Scripts/Items/FireScript.cs
@@ -1,5 +1,6 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FireScript : MonoBehaviour
 {
@@ -9,12 +10,13 @@ public class FireScript : MonoBehaviour
     [SerializeField] private float _maxTimeToExtend = 5;
     [SerializeField] private GameObject _fire;
     private float _timeToExtend;
-
+    static public NavMeshPlus.Components.NavMeshSurface navMeshSurface;
     private float time = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         SetRandomTime();
+        navMeshSurface = FindAnyObjectByType<NavMeshPlus.Components.NavMeshSurface>();
     }
 
     // Update is called once per frame
@@ -26,9 +28,39 @@ public class FireScript : MonoBehaviour
             if (_numberFire < 25)
             {
                 Vector2 direction = Random.insideUnitCircle.normalized;
-                Vector2 newPosition = (Vector2)transform.position + direction;
-                Instantiate(_fire, newPosition, Quaternion.identity);
-                _numberFire++;
+                Vector3 target = transform.position + (Vector3)direction;
+
+                NavMeshHit hit;
+                Debug.Log(NavMesh.SamplePosition(transform.position, out hit, 0.2f, NavMesh.AllAreas));
+                if (NavMesh.SamplePosition(target, out hit, 1f, NavMesh.AllAreas))
+                {
+                    // Cherche le point navigable le plus proche du feu actuel
+                    NavMeshHit startHit;
+                    if (!NavMesh.SamplePosition(transform.position, out startHit, 2f, NavMesh.AllAreas))
+                    {
+                        Debug.Log("Le feu n'est plus proche du NavMesh");
+                        return;
+                    }
+
+                    NavMeshPath path = new NavMeshPath();
+
+                    bool pathFound = NavMesh.CalculatePath(
+                        startHit.position,
+                        hit.position,
+                        NavMesh.AllAreas,
+                        path
+                    );
+
+                    Debug.Log($"PathFound : {pathFound}");
+                    Debug.Log($"Status : {path.status}");
+
+                    if (pathFound && path.status == NavMeshPathStatus.PathComplete)
+                    {
+                        Instantiate(_fire, hit.position, Quaternion.identity);
+                        navMeshSurface.BuildNavMesh();
+                        _numberFire++;
+                    }
+                }
             }
             
             time = 0f;
