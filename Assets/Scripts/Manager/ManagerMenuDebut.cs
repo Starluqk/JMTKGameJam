4cd7b56b5ecf52477c1cc06e59743a5a59d6e740
erwanpianta <erwanpianta1706@gmail.com>
2026-07-23 22:28:44 +0200

    V0.31 - transition entre les plans + nouvelle scene finale

--- Assets/Scripts/Manager/ManagerMenuDebut.cs
+++ Assets/Scripts/Manager/ManagerMenuDebut.cs
@@ -1,15 +1,31 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ManagerMenuDebut : MonoBehaviour
 {
+    public Animator transition;
     public void OnStart()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+        LoadNextScene();
     }
 
     public void OnApplicationQuit()
     {
         Application.Quit();
+    }
+
+    public void LoadNextScene()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(levelIndex);
     }
 }
