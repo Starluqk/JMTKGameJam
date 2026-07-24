89410ea545d75c0de7435aac0005922b4b32a7dc
Nicolay Coretin <corentin.nicolay@student.hepl.be>
2026-07-24 13:54:19 +0200

    V0.34 - début extinctor

--- Assets/Scripts/Items/Extinctor.cs
+++ Assets/Scripts/Items/Extinctor.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Extinctor : MonoBehaviour
 {
+    [SerializeField] private Transform _playerTransform;
+
+    [SerializeField] private GameObject _spray;
+    private Vector3 direction;
+    private VisualEffect _vfxSpray;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _vfxSpray = _spray.GetComponent<VisualEffect>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(1))
+        {
+            _spray.SetActive(true);
+            direction =transform.position -  _playerTransform.position;
+            direction.z = 0;
+            _vfxSpray.SetVector3("Spray", direction);
+        }
+        else
+        {
+            _spray.SetActive(false);
+        }
     }
 }
